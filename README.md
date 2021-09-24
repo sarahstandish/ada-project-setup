78276b009b92e8b2c1262e1ce109e8973ed47783
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-24 12:51:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Ada Python Project Setup
+
+## Description
+A shell script to automate the setup of Ada Developer's Academy Python projects, completed in preparation for a lightning talk (September 2021).
+
+## Author
+[Sarah Standish](https://github.com/sarahstandish)
+
+# Instructions
+
+## One-time setup
+1. Clone the `ada-project-setup` repo into your `projects` directory.
+
+2. `cd` into the `ada-projects-setup` directory.
+
+3. Run the following command in your terminal:
+   `chmod +x python-project.sh`
+
+## Usage
+Set up an Ada Python project by navigating to your `projects` directory and then running the following command in your terminal:
+`ada-project-setup/python-project-setup.sh <clone code for the repository here>`
+
+Example:
+`ada-project-setup/python-project.sh https://github.com/user-name/repository-name.git`
+
---
+++ python-project.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# sets up an Ada Python project
+
+repo=${1}
+
+# check if $repo variable is empty
+if [ -z "$repo" ]
+
+then
+    # give directions on how to run the script if no repo provided
+    echo "When you run the file, you must also specify the repo you want to clone."
+    echo "Type the following into the command line, for example:"
+    echo "ada-project-setup/python-project.sh https://github.com/user-name/repository-name.git"
+
+    exit
+
+else
+    git clone $repo
+
+    # regex to get the directory_name name, thank you Internet commenters (source: https://serverfault.com/questions/417241/extract-repository-name-from-github-url-in-bash)
+    directory_name=$(basename $repo .git)
+
+    # navigate to the directory
+    cd $directory_name
+
+    # setup the virtual environment
+    python3 -m venv venv
+    source venv/bin/activate
+
+    # install project requirements
+    pip install -r requirements.txt
+
+    #for some reason I always get a reminder to upgrade pip no matter how many times I upgrade it so I'll just put that here
+    pip install --upgrade pip 
+fi
